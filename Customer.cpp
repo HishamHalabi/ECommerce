0e11e9db4eeb7114e935622117c3e144997f35f0
HishamHalabi <159281419+HishamHalabi@users.noreply.github.com>
2025-07-04 16:37:48 +0300

    Create Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <Cart.cpp>
+using namespace std;
+const int CurrenYear = 2025 ;
+
+class Customer   {
+    string name;
+    double balance  ;
+    Cart mycart ;
+public  :
+
+    Customer() {
+        balance = 0 ;
+    }
+
+    Customer(string name  , double balance)  {
+        this-> name=name ;
+        this->balance = balance ;
+    }
+
+    double getBalance()  {
+        return balance ;
+    }
+
+    string getName()  {
+        return name ;
+}
+
+    void addProduct(Product &product , int quantity)  {
+           mycart.add(product  ,  quantity) ;      //it will check for availability
+    }
+
+    void Return (Product&product ,  int quantity)  {
+        mycart.Return(product  ,  quantity) ;
+    }
+
+
+    void CheckOut()  {
+        cout<<"** Checkout receipt **\n";
+
+        auto products   =  mycart.getProducts() ;
+        if (products.empty())  {
+                   cout<<"Cart is empty\n" ;
+                   return  ;
+        }
+
+        ShippingService ship(products) ;
+        long double totalCost  =  mycart.getTotalCost()   +   ship.getCost() ;
+
+        if(balance <  totalCost)  {
+              cout<<"insufficent balance\n" ;
+              return  ;
+        }
+
+        for (int i = 0 ;  i <products.size() ;  ++i)  {
+                 if (products[i].first.getExpirationYear()  >=CurrenYear)  {
+                              cout<<"Product "<<products[i].first.getName() <<" is  expired u can return it again\n" ;
+                 }
+        }
+
+        balance-= totalCost ;
+
+
+        ship.display() ;
+        cout<<"\n_________________________\n\n" ;
+
+        long double subtotal  = 0 ;
+        for (int i = 0 ; i  < products.size() ;  ++i) {
+            int cnt = products[i].second ;
+            cout<<cnt <<"X " <<products[i].first.getName() <<" "<<cnt  * products[i].first.getPrice() <<"\n " ;
+            subtotal+=cnt * products[i].first.getPrice() ;
+        }
+        cout<<"____________________/nSubtotal  "<<subtotal <<"\n";
+        cout<<"Shippment  " << ship.getCost() <<"\n";
+        cout<<"Amount  "<<subtotal  + ship.getCost() <<"\n";
+    }
+
+
+
+};
